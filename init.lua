54896c0f5f0de6a1f0273726758e59407c4c7f13
Vlad Gorenkin <vladgorenkin@yandex.ru>
2018-02-07 13:29:02 +0600

    Refactor librarian to be more like built-in awesome libraries

--- init.lua
+++ init.lua
@@ -25,198 +25,198 @@ local awful = require("awful")
 local gears = require("gears")
 local naughty = require("naughty")
 
-return {
-  __libraries = { },
-  verbose = false,
+local librarian = {}
 
-  __notify = function(self, options)
-    if (not self.verbose) then return end
+local libraries = {}
+local verbose = false;
 
-    return naughty.notify(options)
+local notify = function(options)
+  if (not verbose) then return end
+
+  return naughty.notify(options)
+end
+
+local spawn_synchronously = function(command)
+  local handle = io.popen(command)
+  local output = handle:read("*all")
+  output = output:gsub("%c$", "")
+  handle:close()
+
+  return output
+end
+
+local dir_is_empty = function(dir_path)
+  return spawn_synchronously("ls -A " .. dir_path) == ""
+end
+
+local remove_file_or_dir = function(path)
+  os.execute("rm -rf " .. path)
+end
+
+local has_item = function(table, wanted_item)
+  for _, item in pairs(table) do
+    if (item == wanted_item) then
+      return true
+    end
+  end
+
+  return false
+end
+
+local git = {
+  __libraries_path = "",
+
+  clone = function(self, library_name, callback)
+    local path_to_library = self.__libraries_path .. library_name .. "/"
+
+    local command ="git clone https://github.com/" .. library_name .. ".git"
+
+    command = command .. " " .. path_to_library
+    if (callback) then
+      awful.spawn.easy_async_with_shell(command, callback)
+
+      return
+    end
+    awful.spawn.with_shell(command)
   end,
 
-  __spawn_synchronously = function(self, command)
-    local handle = io.popen(command)
-    local output = handle:read("*all")
-    output = output:gsub("%c$", "")
-    handle:close()
+  checkout = function(self, library_name, reference, callback)
+    local path_to_library = self.__libraries_path .. library_name .. "/"
 
-    return output
+    local command = "cd " .. path_to_library .. " && git checkout " .. reference
+    if (callback) then
+      awful.spawn.easy_async_with_shell(command, callback)
+
+      return
+    end
+    awful.spawn.with_shell(command)
   end,
 
-  __dir_is_empty = function(self, dir_path)
-    return self:__spawn_synchronously("ls -A " .. dir_path) == ""
-  end,
+  pull = function(self, library_name, callback)
+    local path_to_library = self.__libraries_path .. library_name .. "/"
 
-  __remove_file_or_dir = function(self, path)
-    os.execute("rm -rf " .. path)
-  end,
+    local command = "cd " .. path_to_library .. " && git pull"
+    if (callback) then
+      awful.spawn.easy_async_with_shell(command, callback)
 
-  __has_item = function(self, table, wanted_item)
-    for _, item in pairs(table) do
-      if (item == wanted_item) then
-        return true
+      return
+    end
+    awful.spawn.with_shell(command)
+  end,
+}
+
+function librarian.update(library_name)
+  local notification = notify({
+      title = "Librarian",
+      text = "Updating " .. library_name .. "...",
+      timeout = 0,
+    })
+
+  git:pull(library_name, function(stdout)
+    local message = library_name .. " was updated."
+    if (stdout:gsub("%c", "") == "Already up to date.") then
+      message = library_name .. " is up to date."
+    end
+    naughty.replace_text(notification, "Librarian", message)
+    naughty.reset_timeout(notification, 5)
+  end)
+end
+
+function librarian.update_all()
+  for _, library_name in pairs(libraries) do
+    librarian.update(library_name)
+  end
+end
+
+function librarian.is_installed(library_name)
+  -- @see https://stackoverflow.com/a/40195356
+  local exists = function (file)
+    local ok, err, code = os.rename(file, file)
+    if not ok then
+      if code == 13 then return true end
+    end
+    return ok
+  end
+
+  local config_dir = gears.filesystem.get_configuration_dir()
+
+  return exists(config_dir .. "libraries/" .. library_name .. "/init.lua")
+end
+
+function librarian.clean()
+  notify({
+      title = "Librarian",
+      text = "Removing not used libraries...",
+    })
+
+  local libraries_dir = gears.filesystem.get_configuration_dir() .. "libraries/"
+
+  local find_command = "cd " .. libraries_dir .. " && "
+  find_command = find_command .. "find -mindepth 2 -maxdepth 2 -type d"
+
+  awful.spawn.easy_async_with_shell(find_command, function(stdout)
+    -- Remove preceding './'s
+    local dir_list = stdout:gsub("%./", "")
+
+    for dir in dir_list:gmatch("(.-)%c") do
+      if (not has_item(self.__libraries, dir)) then
+        notify({
+            title = "Librarian",
+            text = "Removing " .. dir .. "...",
+            timeout = 1,
+          })
+        remove_file_or_dir(libraries_dir .. dir)
+
+        local parent_dir = libraries_dir .. dir:gsub("[^/]+$", "")
+        if (dir_is_empty(parent_dir)) then
+          remove_file_or_dir(parent_dir)
+        end
       end
     end
+  end)
+end
 
-    return false
-  end,
+function librarian.require(library_name, options)
+  table.insert(libraries, library_name)
 
-  __git = {
-    __libraries_path = "",
+  if (not options) then
+    options = {
+      reference = "master",
+    }
+  end
 
-    clone = function(self, library_name, callback)
-      local path_to_library = self.__libraries_path .. library_name .. "/"
-
-      local command ="git clone https://github.com/" .. library_name .. ".git"
-
-      command = command .. " " .. path_to_library
-      if (callback) then
-        awful.spawn.easy_async_with_shell(command, callback)
-
-        return
-      end
-      awful.spawn.with_shell(command)
-    end,
-
-    checkout = function(self, library_name, reference, callback)
-      local path_to_library = self.__libraries_path .. library_name .. "/"
-
-      local command = "cd " .. path_to_library .. " && git checkout " .. reference
-      if (callback) then
-        awful.spawn.easy_async_with_shell(command, callback)
-
-        return
-      end
-      awful.spawn.with_shell(command)
-    end,
-
-    pull = function(self, library_name, callback)
-      local path_to_library = self.__libraries_path .. library_name .. "/"
-
-      local command = "cd " .. path_to_library .. " && git pull"
-      if (callback) then
-        awful.spawn.easy_async_with_shell(command, callback)
-
-        return
-      end
-      awful.spawn.with_shell(command)
-    end,
-  },
-
-  update = function(self, library_name)
-    local notification = self:__notify({
+  if (not librarian.is_installed(library_name)) then
+    local notification = notify({
         title = "Librarian",
-        text = "Updating " .. library_name .. "...",
+        text = "Installing " .. library_name .. " library...",
         timeout = 0,
       })
 
-    self.__git:pull(library_name, function(stdout)
-      local message = library_name .. " was updated."
-      if (stdout:gsub("%c", "") == "Already up to date.") then
-        message = library_name .. " is up to date."
-      end
-      naughty.replace_text(notification, "Librarian", message)
+    git:clone(library_name, function()
+      naughty.replace_text(notification, "Librarian", library_name .. " is installed.")
       naughty.reset_timeout(notification, 5)
     end)
-  end,
 
-  update_all = function(self)
-    for _, library_name in pairs(self.__libraries) do
-      self:update(library_name)
-    end
-  end,
-
-  is_installed = function(self, library_name)
-    -- @see https://stackoverflow.com/a/40195356
-    local exists = function (file)
-      local ok, err, code = os.rename(file, file)
-      if not ok then
-        if code == 13 then return true end
-      end
-      return ok
+    if (options.reference ~= "master") then
+      git:checkout(library_name, options.reference)
     end
 
-    local config_dir = gears.filesystem.get_configuration_dir()
+    return nil
+  end
 
-    return exists(config_dir .. "libraries/" .. library_name .. "/init.lua")
-  end,
+  local config_dir = gears.filesystem.get_configuration_dir()
+  local author = string.match(library_name, "[^/]+")
+  package.path = config_dir .. "libraries/" .. author .. "/?/init.lua;" .. package.path
+  package.path = config_dir .. "libraries/" .. author .. "/?.lua;" .. package.path
 
-  clean = function(self)
-    self:__notify({
-        title = "Librarian",
-        text = "Removing not used libraries...",
-      })
+  return require('libraries/' .. library_name)
+end
 
-    local libraries_dir = gears.filesystem.get_configuration_dir() .. "libraries/"
+function librarian.init(options)
+  verbose = options.verbose or false
 
-    local find_command = "cd " .. libraries_dir .. " && "
-    find_command = find_command .. "find -mindepth 2 -maxdepth 2 -type d"
+  local libraries_path = gears.filesystem.get_configuration_dir() .. "libraries/"
+  git.__libraries_path = libraries_path
+end
 
-    awful.spawn.easy_async_with_shell(find_command, function(stdout)
-      -- Remove preceding './'s
-      local dir_list = stdout:gsub("%./", "")
-
-      for dir in dir_list:gmatch("(.-)%c") do
-        if (not self:__has_item(self.__libraries, dir)) then
-          self:__notify({
-              title = "Librarian",
-              text = "Removing " .. dir .. "...",
-              timeout = 1,
-            })
-          self:__remove_file_or_dir(libraries_dir .. dir)
-
-          local parent_dir = libraries_dir .. dir:gsub("[^/]+$", "")
-          if (self:__dir_is_empty(parent_dir)) then
-            self:__remove_file_or_dir(parent_dir)
-          end
-        end
-      end
-    end)
-  end,
-
-  require = function(self, library_name, options)
-    table.insert(self.__libraries, library_name)
-
-    if (not options) then
-      options = {
-        reference = "master",
-      }
-    end
-
-    if (not self:is_installed(library_name)) then
-      local notification = self:__notify({
-          title = "Librarian",
-          text = "Installing " .. library_name .. " library...",
-          timeout = 0,
-        })
-
-      self.__git:clone(library_name, function()
-        naughty.replace_text(notification, "Librarian", library_name .. " is installed.")
-        naughty.reset_timeout(notification, 5)
-      end)
-
-      if (options.reference ~= "master") then
-        self.__git:checkout(library_name, options.reference)
-      end
-
-      return nil
-    end
-
-    self.__git:checkout(library_name, options.reference)
-
-    local config_dir = gears.filesystem.get_configuration_dir()
-    local author = string.match(library_name, "[^/]+")
-    package.path = config_dir .. "libraries/" .. author .. "/?/init.lua;" .. package.path
-    package.path = config_dir .. "libraries/" .. author .. "/?.lua;" .. package.path
-
-    return require('libraries/' .. library_name)
-  end,
-
-  init = function(self, options)
-    self.verbose = options.verbose or false
-
-    local libraries_path = gears.filesystem.get_configuration_dir() .. "libraries/"
-    self.__git.__libraries_path = libraries_path
-  end,
-}
+return librarian
