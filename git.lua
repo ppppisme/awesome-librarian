fe700df0fe6f47031481a8faa744280bcbc4f427
Vladislav Gorenkin <gorenkin@ruvents.com>
2020-10-03 13:10:31 +0600

    Specify locale and no_prompt for git command

--- git.lua
+++ git.lua
@@ -6,8 +6,10 @@ local git = {}
 local libraries_dir = ""
 local is_init = false
 
+local git_command = "GIT_TERMINAL_PROMPT=0 LC_ALL=en_US.UTF-8 git"
+
 function git.clone(library_name, url, callback)
-  local command = "git clone "
+  local command = git_command .. " clone "
   url = url or "https://github.com/" .. library_name .. ".git"
   command = command .. url
 
@@ -25,7 +27,7 @@ end
 
 function git.checkout(library_name, reference, callback)
   local path_to_library = libraries_dir .. library_name .. "/"
-  local command = "cd " .. path_to_library .. " && git checkout " .. reference
+  local command = "cd " .. path_to_library .. " && " .. git_command .. " checkout " .. reference
 
   if (callback) then
     awful.spawn.easy_async_with_shell(command, callback)
@@ -38,7 +40,7 @@ end
 
 function git.pull(library_name, callback)
   local path_to_library = libraries_dir .. library_name .. "/"
-  local command = "cd " .. path_to_library .. " && git pull"
+  local command = "cd " .. path_to_library .. " && " .. git_command .. " pull"
 
   if (callback) then
     awful.spawn.easy_async_with_shell(command, callback)
